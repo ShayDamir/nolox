1a6a4f4c742772a675b4cefd8abc8a4a3093ad4b
Damir Shaikhutdinov <lost404@gmail.com>
2020-05-13 08:53:37 +0200

    src: Implement dummy custom test framework.
    Add a simple test framework that just runs all the tests.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x20,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    // println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
 }
